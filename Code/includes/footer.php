058df770c64a82bfcc60ca0c306b4094ae5539a8
Habayatou <105522094+Habayatou@users.noreply.github.com>
2024-02-26 13:44:43 +0000

    Delete Code directory

--- Code/includes/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="footer">
-					<div class="footer-wrap">
-						<div class="d-sm-flex justify-content-center justify-content-sm-between">
-						<span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © <a href="https://www.bootstrapdash.com/" target="_blank">AfMAR.org</a> 2024</span>
-						<span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">African Mitigation and Adaptation Review Plateform </span>
-						</div>
-					</div>
-				</footer>
